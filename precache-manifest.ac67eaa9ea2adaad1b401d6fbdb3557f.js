3096cd485a4ecaeb01f64ff94da7315be2cba2d1
Alla Sariieva <alla.cleo@gmail.com>
2019-10-16 15:05:09 +0300

    Updates

---
+++ precache-manifest.ac67eaa9ea2adaad1b401d6fbdb3557f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f979d76f47dcff94a79e71b5914eeab",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "94ba0254243d0412baa3",
+    "url": "/goit-react-hw-01-components/static/css/main.c20b6aad.chunk.css"
+  },
+  {
+    "revision": "1d7160eb9f8da62b93aa",
+    "url": "/goit-react-hw-01-components/static/js/2.162f95ae.chunk.js"
+  },
+  {
+    "revision": "94ba0254243d0412baa3",
+    "url": "/goit-react-hw-01-components/static/js/main.a4c5fbf9.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
